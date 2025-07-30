a7a93b8d3fb764ea689bd183c4473c96370a0e52
J van Leipsig <20169355+missihawk@users.noreply.github.com>
2025-07-30 16:00:01 +0200

    Added logging and performance measurments.

--- src/main.py
+++ src/main.py
@@ -1,18 +1,34 @@
 from yf_driver import start_driver, open_yf, close_driver
 from fetch.financials import fetch_yf_statement
 from storage.local_data import load_data, merge_statement_data, save_data
+import logging
+import time
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 driver = start_driver(True)
 open_yf(driver)  # cookies accepteren
 
 for ticker in ["AXS.AS", "ACOMO.AS", "ALFEN.AS", "BFIT.AS", "BMW3.DE", "DTE.DE", "FNTN.DE", "BOSS.DE", "KENDR.AS", "MBG.DE", "NEDAP.AS", "SLIGR.AS", "TKA.DE", "VLK.AS", "VOW3.DE", "ZAL.DE"]:
-    data = load_data(ticker)
+    logging.info(f"Start processing: {ticker}")
+    start_time = time.perf_counter()
+    
+    try:
+        data = load_data(ticker)
 
-    for report_type in ["FY", "Q"]:
-        for statement_type in ["financials", "balance-sheet", "cash-flow"]:
-            df = fetch_yf_statement(driver, ticker, statement_type, report_type)
-            data = merge_statement_data(data, df, statement_type, report_type)
+        for report_type in ["FY", "Q"]:
+            for statement_type in ["financials", "balance-sheet", "cash-flow"]:
+                df = fetch_yf_statement(driver, ticker, statement_type, report_type)
+                data = merge_statement_data(data, df, statement_type, report_type)
 
-    save_data(ticker, data)
+        save_data(ticker, data)
+    except Exception as e:
+        logging.error(f"Error while processing {ticker}: {e}")
+
+    end_time = time.perf_counter()
+    logging.info(f"Finished {ticker} in {end_time - start_time:.2f} seconds\n")
 
 close_driver(driver)
