c44590a54e172c00232c95da9f7b47c30dcba8b3
J van Leipsig <20169355+missihawk@users.noreply.github.com>
2025-07-30 12:19:51 +0200

    More and better noise supression.

--- src/yf-data-extract.py
+++ src/yf-data-extract.py
@@ -13,6 +13,7 @@ def start_driver():
     options = Options()
     options.add_argument("--headless=new")
     options.add_argument("--disable-gpu")
+    options.add_argument("--use-gl=swiftshader")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("window-size=1920,1080")
